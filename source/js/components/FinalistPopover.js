9c6c9dcc772c536ce134303ac681e1316e710a22
jlengstorf <jason.lengstorf@copterlabs.com>
2016-02-19 16:32:28 +0900

    feat(cleanup): refactored for legibility/DRYness

--- source/js/components/FinalistPopover.js
+++ source/js/components/FinalistPopover.js
@@ -6,77 +6,62 @@ function FinalistPopover({app, clickHandler}) {
     return <div />;
   }
 
-  const finalist = {};
+  const finalist = {
+    age: setIfKeyPathExists(app, ['finalist', 'age']),
+    images: setIfKeyPathExists(app, ['finalist', 'images']),
+    weight: setIfKeyPathExists(app, ['finalist', 'weight']),
+    inches: setIfKeyPathExists(app, ['finalist', 'inches']),
+    bodyfat: setIfKeyPathExists(app, ['finalist', 'bodyfat']),
+  };
 
-  if (app.hasIn(['finalist', 'age'])) {
-    finalist.age = app.getIn(['finalist', 'age']);
-  }
-
-  if (app.hasIn(['finalist', 'images'])) {
-    finalist.images = app.getIn(['finalist', 'images']);
-  }
-
-  if (app.hasIn(['finalist', 'weight'])) {
-    finalist.weight = app.getIn(['finalist', 'weight']);
-  }
-
-  if (app.hasIn(['finalist', 'inches'])) {
-    finalist.inches = app.getIn(['finalist', 'inches']);
-  }
-
-  if (app.hasIn(['finalist', 'bodyfat'])) {
-    finalist.bodyfat = app.getIn(['finalist', 'bodyfat']);
-  }
+  const images = ['front', 'side', 'back'].map(image => {
+    return finalist.images[image] && (
+      <img src={finalist.images[image]}
+           key={image}
+           className={`pn-finalists__image pn-finalists__image--${image}`} />
+    );
+  });
 
   return (
     <div className={classes.join(' ')}>
       <div className="pn-finalists__transformation">
         {finalist.images &&
         <div className="pn-finalists__image-wrapper">
-          {finalist.images.front &&
-            <img
-              className="pn-finalists__image pn-finalists__image--front"
-              src={finalist.images.front} />
-          }
-          {finalist.images.side &&
-            <img
-              className="pn-finalists__image pn-finalists__image--side"
-              src={finalist.images.side} />
-          }
-          {finalist.images.back &&
-            <img
-              className="pn-finalists__image pn-finalists__image--back"
-              src={finalist.images.back} />
-          }
+          {images}
         </div>}
         <div className="pn-finalists__info-wrapper">
           <ul className="pn-finalists__info">
             <li className="pn-finalists__info-item pn-finalists__info-item--type">
               $250,000 Finalist
             </li>
+
             {finalist.age &&
             <li className="pn-finalists__info-item pn-finalists__info-item--age">
               <strong className="pn-finalists__info-label">Age:</strong>
-              {app.getIn(['finalist', 'age'])} years
+              {finalist.age} years
             </li>}
+
             {finalist.weight &&
             <li className="pn-finalists__info-item pn-finalists__info-item--weight">
               <strong className="pn-finalists__info-label">Weight Lost:</strong>
               {roundToNearestTenth(finalist.weight.start - finalist.weight.end)} lbs
               <small className="pn-finalists__info-note">(from {finalist.weight.start} lbs to {finalist.weight.end} lbs)</small>
             </li>}
+
             {finalist.bodyfat &&
             <li className="pn-finalists__info-item pn-finalists__info-item--bodyfat">
               <strong className="pn-finalists__info-label">% Body Fat Lost:</strong>
               {roundToNearestTenth(finalist.bodyfat.start - finalist.bodyfat.end)}%
               <small className="pn-finalists__info-note">(from {finalist.bodyfat.start}% to {finalist.bodyfat.end}%)</small>
             </li>}
+
             {finalist.inches &&
             <li className="pn-finalists__info-item pn-finalists__info-item--girth">
               <strong className="pn-finalists__info-label">Total Inches Lost:</strong>
               {roundToNearestTenth(finalist.inches.start - finalist.inches.end)} inches
               <small className="pn-finalists__info-note">(from {finalist.inches.start} inches to {finalist.inches.end} inches)</small>
             </li>}
+
           </ul>
         </div>
       </div>
@@ -88,6 +73,14 @@ function FinalistPopover({app, clickHandler}) {
 }
 
 export default FinalistPopover;
+
+function setIfKeyPathExists(app, keyPath) {
+  if (app.hasIn(keyPath)) {
+    return app.getIn(keyPath);
+  }
+
+  return false;
+}
 
 function roundToNearestTenth(num) {
   return Math.round(num * 10) / 10;

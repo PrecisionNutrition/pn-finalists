e135fd1065610e5afdb9a74348dfe23edf57a977
jlengstorf <jason.lengstorf@copterlabs.com>
2016-02-19 16:32:15 +0900

    feat(portal): put the popover inside a portal

--- source/js/components/PNFinalistsApp.js
+++ source/js/components/PNFinalistsApp.js
@@ -19,6 +19,7 @@ import {dispatch} from '../dispatcher';
 import AppStore from '../stores/AppStore';
 import FinalistsStore from '../stores/FinalistsStore';
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+import Portal from '../components/Portal';
 import FinalistBox from '../components/FinalistBox';
 import FinalistPopover from '../components/FinalistPopover';
 
@@ -42,20 +43,22 @@ class PNFinalists extends Component {
     return (
       <div className="pn-finalists">
         <FinalistBox finalists={this.state.finalists} clickHandler={togglePopoverVisibility} />
-        <ReactCSSTransitionGroup
-          transitionName="pn-finalists__popover-"
-          transitionAppear={true}
-          transitionAppearTimeout={200}
-          transitionEnterTimeout={200}
-          transitionLeaveTimeout={200}
-        >
-          {this.state.app.get('showPopover') &&
-          <FinalistPopover
-            key="popover"
-            app={this.state.app}
-            clickHandler={togglePopoverVisibility}
-          />}
-        </ReactCSSTransitionGroup>
+        <Portal portalId="pn-finalists-popover" className="pn-finalists">
+          <ReactCSSTransitionGroup
+            transitionName="pn-finalists__popover-"
+            transitionAppear={true}
+            transitionAppearTimeout={200}
+            transitionEnterTimeout={200}
+            transitionLeaveTimeout={200}
+          >
+            {this.state.app.get('showPopover') &&
+            <FinalistPopover
+              key="popover"
+              app={this.state.app}
+              clickHandler={togglePopoverVisibility}
+            />}
+          </ReactCSSTransitionGroup>
+        </Portal>
       </div>
     );
   }
